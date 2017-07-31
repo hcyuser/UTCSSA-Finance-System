afcb73138efba81fd06f968cd22c7024ae9f1b1d
CHIH-YANG HUANG <user@hcy.idv.tw>
2017-07-31 23:20:15 +0800

    Modify the DateTimeField
    Change the date field to fill automatically

--- blog/article/models.py
+++ blog/article/models.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 from django.db import models
-
+from datetime import datetime
 class Category(models.Model):
     name = models.CharField(u'Name', max_length=50)
 
@@ -11,7 +11,7 @@ class Category(models.Model):
 class Article(models.Model):
     title = models.TextField(u'購買品項(EX: 蘇打餅X1)')
     uid = models.CharField(u'學號',default='0',max_length=10)
-    filldate = models.CharField(u'填寫日期YYYYMMDD',default='00000000',max_length=8)
+    filldate =  models.DateTimeField(u'Date YYYY-MM-DD',default=datetime.now)
     applycost = models.CharField(u'請款金額',default='0',max_length=8)
     content = models.TextField(u'事由')
 
